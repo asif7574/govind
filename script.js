acfa160de6fc9e8dc35d60d3d9d8c4c69caab4f2
N ASIFKHAN <asifkhan4092@gmail.com>
2024-08-25 20:09:59 +0530

    govind

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function govind(){
+    
+
+    document.getElementById('cont').innerHTML=''   
+    const ele1=document.createElement('img')
+    ele1.src='./govind.jpg'
+    ele1.style.height='300px'
+
+    const ele2=document.createElement('h2')
+ele2.innerHTML='I am a LOKA VANAM'
+    document.getElementById('cont').appendChild(ele2)
+    document.getElementById('cont').appendChild(ele1)
+
+    }
+
